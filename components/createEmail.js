f98e97637389a2892026ee230551d8566535098b
Awardxz <jonkovaqi8@gmail.com>
2024-10-03 20:30:25 +0200

    new insane optimization

---
+++ components/createEmail.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const headers = require("../info/headers.js");
+
+
+async function createEmail(email) {
+  let data = JSON.stringify({
+    email: `${email}`,
+  });
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://order.surfshark.com/api/v1/account/users?source=one",
+    headers,
+    data: data,
+  };
+
+  try {
+    const response = await axios.request(config);
+    console.log("Email Created");
+    console.log(JSON.stringify(response.data));
+  } catch (error) {
+    console.error(error); // Use `console.error` for logging errors
+  }
+}
+
+module.exports = { createEmail }
--- index.js
+++ index.js
@@ -1,106 +1,72 @@
-const puppeteer = require('puppeteer-extra')
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
-// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const { createEmail } = require("./components/createEmail");
+const { resetPassword } = require("./components/resetPassword");
+
 puppeteer.use(StealthPlugin());
 
- function delay(params) {
-  return new Promise(resolve => setTimeout(resolve , params))
+function delay(params) {
+  return new Promise((resolve) => setTimeout(resolve, params));
 }
 
 (async () => {
   const password = "WaledGamer123!";
 
-  const browser = await puppeteer.launch({ headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const emailPage = await browser.newPage();
-  const customUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
- 
+  const customUA =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+
   // Set custom user agent
   await emailPage.setUserAgent(customUA);
-  
+
   await emailPage.goto("https://temp-mail.io/en");
-  
+
   await emailPage.waitForSelector(".email__input");
-  await delay(150)
-  
+  await delay(150);
+
   const email = await emailPage.evaluate(() => {
-      let inputElement = document.querySelector('#email');
-      return inputElement.getAttribute('title');
+    let inputElement = document.querySelector("#email");
+    return inputElement.getAttribute("title");
   });
 
   console.log(email);
-  
-  const page = await browser.newPage();
-  await page.goto("https://order.surfshark.com/?frequency=24");
-  await page.setUserAgent(customUA);
-  console.log("Going to Website");
-  
-  await page.setViewport({ width: 1080, height: 1024 });
-  await page.waitForSelector(".YoSuD.Zswp6");
-  await page.type(".YoSuD.Zswp6", email);
-  console.log("Email Typed");
+  if (email == null) return false;
+  await createEmail(email);
+  await delay(1000)
+  await resetPassword(email);
 
-  const searchResultSelector = "div.lLyrv > div.bj21b > div > div.Ewm0n > div.XgKPH > div.dFvXQ > div.KolGQ > div > div:nth-child(5)";
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  console.log("Picking Crypto");
-  
-  const payment = "div.lLyrv > div.bj21b > div > div.Ewm0n > div.XgKPH > div.dFvXQ > div.KolGQ > div > div.wT2mk.tOuu9 > div.Wfune.tOuu9 > div > div.g2nAY > button";
-  await page.waitForSelector(payment);
-  await page.click(payment);
-
-  console.log("Completed Payment");
-  
-  const passwordPage = await browser.newPage();
-  await passwordPage.goto("https://my.surfshark.com/auth/reset");
-  
-  await passwordPage.waitForSelector("body div#root div.lertL div.zVuif div#login-content.btnA5 div.ira6n div.hQjhW.cGoR9 div.E_enA div.Pq283 form.z85m3 div.WSbPR div.FhOcs.shbyo div.TZwqa input.YoSuD.Zswp6");
-  await passwordPage.type("body div#root div.lertL div.zVuif div#login-content.btnA5 div.ira6n div.hQjhW.cGoR9 div.E_enA div.Pq283 form.z85m3 div.WSbPR div.FhOcs.shbyo div.TZwqa input.YoSuD.Zswp6", email);
-  
-  console.log("Adding Password");
-  
-  const passwordReset = "#resetSubmit";
-  await passwordPage.waitForSelector(passwordReset);
-  setTimeout(() => {
-      passwordPage.click(passwordReset);
-  }, 500);
-  
-  console.log("Password Resetting");
-  setTimeout(() => {
-      emailPage.bringToFront();
-  }, 900);
-  
   await emailPage.waitForSelector(".message__title");
   await emailPage.click(".message__title");
-  
+
   await emailPage.waitForSelector('a[href*="links.surfshark.com/u/click"]');
-  
+
   const clickPage = await emailPage.evaluate(() => {
-    let element = document.querySelector('a[href*="links.surfshark.com/u/click"]');
-    return element.getAttribute('href');
-});
+    let element = document.querySelector(
+      'a[href*="links.surfshark.com/u/click"]'
+    );
+    return element.getAttribute("href");
+  });
 
-console.log(clickPage);  // This will print the href value
+  console.log(clickPage); 
 
-// Open the link in a new page
-const newPage = await browser.newPage();
-await newPage.goto(clickPage);
+  const newPage = await browser.newPage();
+  await newPage.goto(clickPage);
 
-await newPage.waitForSelector('.YoSuD.F8m7M.Zswp6');
-await newPage.type(".YoSuD.F8m7M.Zswp6", password);
-await newPage.waitForSelector(".Dmuut");
-setTimeout(() => {
-   newPage.click(".Dmuut");
-}, 1500);
+  await newPage.waitForSelector(".YoSuD.F8m7M.Zswp6");
+  await newPage.type(".YoSuD.F8m7M.Zswp6", password);
+  await newPage.waitForSelector(".Dmuut");
+  setTimeout(() => {
+    newPage.click(".Dmuut");
+  }, 1500);
 
-console.log("Password Reset Completed");
-  
-setTimeout(() => {
-  browser.close();
-},3000)
-console.clear();
-console.log(`Email: ${email}`);
-console.log(`Password: ${password}`)
+  console.log("Password Reset Completed");
+
+  setTimeout(() => {
+    browser.close();
+  }, 3000);
+  console.clear();
+  console.log(`Email: ${email}`);
+  console.log(`Password: ${password}`);
 })();
